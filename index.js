37f6405b5232f0d5790242a0a1c42ef2988061c2
Simon Ngugi Ngigi <simonngugi88@gmail.com>
2022-08-29 14:51:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+    const info = document.getElementById('text')
+    info.textContent = ``;
+    info.appendChild(document.createTextNode('This is really cool!'))
+  });
